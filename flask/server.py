af3930fab079d2e6e267fb55508bb7e1feecc62f
Mishin Yura <mishin.inbox@gmail.com>
2021-05-18 09:30:04 +0300

    Added the rest of the basic pages. Closes #3

--- flask/server.py
+++ flask/server.py
@@ -3,5 +3,19 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/index.html")
 def home():
     return render_template('index.html')
+
+@app.route("/works.html")
+def works():
+    return render_template('works.html')
+
+@app.route("/about.html")
+def about():
+    return render_template('about.html')
+
+@app.route("/contact.html")
+def contact():
+    return render_template('contact.html')
+
