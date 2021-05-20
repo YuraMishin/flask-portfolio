e243587a4b69befee31c24e831c309445bb21595
Mishin Yura <mishin.inbox@gmail.com>
2021-05-20 10:32:54 +0300

    Implemented a feature to save contacts data into a *.txt file. Closes #6

--- flask/server.py
+++ flask/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -11,3 +11,20 @@ def home():
 @app.route("/<string:page_name>")
 def html_page(page_name):
     return render_template(page_name)
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_file(data)
+        return redirect('/thankyou.html')
+    else:
+        return 'error'
+
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        file = database.write(f'{email},{subject},{message}\n')
