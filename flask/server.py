b016c09892c8f77db800cc9285e146b9c5a03091
Mishin Yura <mishin.inbox@gmail.com>
2021-05-20 11:30:11 +0300

    Implemented a feature to save data as *.csv. Closes #7

--- flask/server.py
+++ flask/server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+import csv
 
 app = Flask(__name__)
 
@@ -16,11 +17,15 @@ def html_page(page_name):
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
-        data = request.form.to_dict()
-        write_to_file(data)
-        return redirect('/thankyou.html')
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('/thankyou.html')
+        except:
+            return 'error'
     else:
         return 'error'
+
 
 def write_to_file(data):
     with open('database.txt', mode='a') as database:
@@ -28,3 +33,14 @@ def write_to_file(data):
         subject = data["subject"]
         message = data["message"]
         file = database.write(f'{email},{subject},{message}\n')
+
+
+def write_to_csv(data):
+    with open('database.csv', 'a', newline='') as csvfile:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+
+        csv_writer = csv.writer(csvfile, delimiter=',',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
